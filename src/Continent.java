f2c7edfe91bf580d047dd1300bf740b9027b2bc6
Andréas BALLIS <balli002@pc056.iut-rcc-info.urca>
2015-06-15 16:30:38 +0200

    work on Continent

--- src/Continent.java
+++ src/Continent.java
@@ -1,6 +1,19 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Continent {
 	private ArrayList<Pays> pays;
+	private int bonusPion;
+	private String nom;
 	
+	public Continent(String nom, int bonus)
+	{
+		this.nom = nom;
+		this.bonusPion = bonus;
+		this.pays = new ArrayList<Pays>();
+	}
+	
+	public void addPays(Pays p)
+	{
+		this.pays.add(p);
+	}
 }
