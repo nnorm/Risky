0ef9b396b6a3f533de840d4b14717c9115a088e9
Thomas COURTOT <court012@pc058.iut-rcc-info.urca>
2015-06-15 16:33:40 +0200

    database

--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,23 @@
+import java.sql.*;
+import oracle.jdbc.OracleDriver;
 
 public class Database {
-
+	private String requete;
+	try{
+	DriverManager.registerDriver (new OracleDriver());
+	Connection vCon = DriverManager.getConnection("jdbc:oracle:thin:infs2_prj06/azerty01@bd11:1521:bd11");
+	Statement vst = vCon.createStatement();
+	ResultSet vRs = vSt.executeQuery("SELECT id_joueur, pseudo, couleur FROM JOUEUR");
+	while(vRs.next()){
+		String id = vRs.getString(1);
+		String pseudo = vRs.getString(2);
+		String couleur = vRs.getString(3);
+		System.out.println(id + pseudo + couleur);
+		
+	}
+	
+	}
+	catch(Exception e){
+		
+	}
 }
