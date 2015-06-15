585be40f460deeb3f7be51601c117ef295ea755f
Stéphane THIERRY <thier001@pc057.iut-rcc-info.urca>
2015-06-15 16:05:05 +0200

    whatevah

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,33 +1,34 @@
-import java.lang.system;
+import java.lang.System;
+import java.util.*;
 
 public class Joueur {
 
 	protected String pseudo;
 	protected int armeesDispo;
-	protected Color couleur;
-	protected List<Carte> main;
-	protected List<Continent> continent;
-	protected List<Pays> pays;
+	//protected Color couleur;
+	protected LinkedList<Carte> main;
+	//protected LinkedList<Continent> continent;
+	protected LinkedList<Pays> pays;
 	
 	public void distribuer(Pays p,int nbPions){
 		if(this.armeesDispo>nbPions){
-		this.pays.ajouterPions(nbPions);
+		p.ajouterPions(nbPions);
 		this.armeesDispo-=nbPions;
-	}
+		}
 	}
 	
-	public combinaison(Carte c1,Carte c2,Carte c3)
+	public int combinaison(Carte c1,Carte c2,Carte c3)
 	{
 		int res=0;
-		if(c1.type==Canon && c2.type==Canon && c3.type==Canon)res=8;
-		else if(c1.type==Soldat && c2.type==Soldat && c3.type==Soldat)res=3;
-		else if(c1.type==Cavalier && c2.type==Cavalier && c3.type==Cavalier)res=5;
-		else if((c1.type==Soldat && c2.type==Cavalier && c3.type==Canon) ||
-				(c1.type==Cavalier && c2.type==Soldat && c3.type==Canon) ||
-				(c1.type==Canon && c2.type==Cavalier && c3.type==Soldat) ||
-				(c1.type==Soldat && c2.type==Canon && c3.type==Cavalier) ||
-				(c1.type==Cavalier && c2.type==Canon && c3.type==Soldat) ||
-				(c1.type==Canon && c2.type==Soldat && c3.type==Cavalier))res=10;
+		if(c1.getType()==TypeCarte.Canon && c2.getType()==TypeCarte.Canon && c3.getType()==TypeCarte.Canon)res=8;
+		else if(c1.getType()==TypeCarte.Soldat && c2.getType()==TypeCarte.Soldat && c3.getType()==TypeCarte.Soldat)res=3;
+		else if(c1.getType()==TypeCarte.Cavalier && c2.getType()==TypeCarte.Cavalier && c3.getType()==TypeCarte.Cavalier)res=5;
+		else if((c1.getType()==TypeCarte.Soldat && c2.getType()==TypeCarte.Cavalier && c3.getType()==TypeCarte.Canon) ||
+				(c1.getType()==TypeCarte.Cavalier && c2.getType()==TypeCarte.Soldat && c3.getType()==TypeCarte.Canon) ||
+				(c1.getType()==TypeCarte.Canon && c2.getType()==TypeCarte.Cavalier && c3.getType()==TypeCarte.Soldat) ||
+				(c1.getType()==TypeCarte.Soldat && c2.getType()==TypeCarte.Canon && c3.getType()==TypeCarte.Cavalier) ||
+				(c1.getType()==TypeCarte.Cavalier && c2.getType()==TypeCarte.Canon && c3.getType()==TypeCarte.Soldat) ||
+				(c1.getType()==TypeCarte.Canon && c2.getType()==TypeCarte.Soldat && c3.getType()==TypeCarte.Cavalier))res=10;
 		return res;
 	}
 	
--- src/Pays.java
+++ src/Pays.java
@@ -1,4 +1,18 @@
 
 public class Pays {
-
+	private String nom;
+	private int nbArmees;
+	//private Continent continent;
+	private Joueur owner;
+	private Pays[] frontaliers;
+	
+	public void ajouterPions(int nbPions)
+	{
+		if(nbPions < this.owner.getArmeesDispo())
+			throw new RuntimeException("requête impossible");
+		
+		this.nbArmees += nbPions;
+		this.owner.enleverArmeesDispo(nbPions);
+	}
+	
 }
