413cfc0b8e5a919077afe9d2ac38f66477b4dd32
Thomas COURTOT <court012@pc058.iut-rcc-info.urca>
2015-06-15 17:25:33 +0200

    update Database

--- src/Database.java
+++ src/Database.java
@@ -1,23 +1,29 @@
 import java.sql.*;
-import oracle.jdbc.OracleDriver;
 
 public class Database {
 	private String requete;
-	try{
-	DriverManager.registerDriver (new OracleDriver());
-	Connection vCon = DriverManager.getConnection("jdbc:oracle:thin:infs2_prj06/azerty01@bd11:1521:bd11");
-	Statement vst = vCon.createStatement();
-	ResultSet vRs = vSt.executeQuery("SELECT id_joueur, pseudo, couleur FROM JOUEUR");
-	while(vRs.next()){
-		String id = vRs.getString(1);
-		String pseudo = vRs.getString(2);
-		String couleur = vRs.getString(3);
-		System.out.println(id + pseudo + couleur);
+	public void sql(){
+		try { 
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Where is your Oracle JDBC Driver?");
+			e.printStackTrace();
+		}
 		
-	}
-	
-	}
-	catch(Exception e){
-		
+		try{
+			Connection vCon = DriverManager.getConnection("jdbc:oracle:thin:@bd11:1521:bd11", "infs2_prj06","azerty01");
+			Statement vSt = vCon.createStatement();
+			ResultSet vRs = vSt.executeQuery("SELECT id_joueur, pseudo, couleur FROM JOUEUR");
+			while(vRs.next();)
+			{
+				String id = vRs.getString(1);
+				String pseudo = vRs.getString(2);
+				String couleur = vRs.getString(3);
+				System.out.println(id + pseudo + couleur);
+			}
+		}
+		catch(Exception e){
+			System.out.print(e);
+		}
 	}
 }
