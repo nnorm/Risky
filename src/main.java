93662e8950a6d4dabd6b41d04ad8c027a57f2214
Andréas BALLIS <balli002@pc056.iut-rcc-info.urca>
2015-06-15 14:28:52 +0200

    inital project commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 
+		// Init game
+		// etc...
+		System.out.println("Bienvenu dans le jeu.");
+	}
+
+}
