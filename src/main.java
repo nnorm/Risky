c1b2232b854e4c560b47b4b7ce9591ac012aa480
Thomas COURTOT <court012@pc082.iut-rcc-info.urca>
2015-06-16 16:12:40 +0200

    update database

--- src/main.java
+++ src/main.java
@@ -9,7 +9,7 @@ public class main {
 		String[] champs = {"id_joueur","pseudo", "couleur"};
 		String[] table = {"JOUEUR"};
 		String[] val = {"J002","Test2","blue"};
-		Database bd = new Database(champs,table,val,"");
-		bd.requeteInsert();
+		Database bd = new Database(champs,table,val,"id_joueur = 'J002'");
+		bd.requeteDelete();
 	}
 }
