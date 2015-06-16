ae6e9558ae244ee691e53018c9c869bc98300aef
Andréas BALLIS <balli002@pc075.iut-rcc-info.urca>
2015-06-16 13:32:07 +0200

    added javadoc and completed some classes too.

--- src/Humain.java
+++ src/Humain.java
@@ -1,6 +1,19 @@
-import java.util.LinkedList;
-
-
+import java.util.*;
+/**
+ * Classe fille de Joueur jouant le rôle de joueur humain. 
+ * */
 public class Humain extends Joueur{
-	
+	/**
+	 * Constructeur avec arguments.
+	 * @param pseudo le pseudo du joueur. (String)
+	 * @param nbArmeesInit le nombre d'armées disponible à l'initialisation du jeu. (int) 
+	 * */
+	public Humain(String pseudo, int nbArmeesInit)
+	{
+		this.pseudo = pseudo;
+		this.armeesDispo = nbArmeesInit;
+		this.main = new LinkedList<Carte>();
+		this.pays = new LinkedList<Pays>();
+		this.continent = new LinkedList<Continent>();
+	}
 }
--- src/TypeCarte.java
+++ src/TypeCarte.java
@@ -1,5 +1,7 @@
+/**
+ * Classe d'énumération. Enumère les différents types de cartes. 
+ * */
 public enum TypeCarte{
-	
 	Soldat,
 	Cavalier,
 	Canon
