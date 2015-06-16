a4d870f737775ba460ecfc31d9276a9d11bf784e
Thomas COURTOT <court012@pc082.iut-rcc-info.urca>
2015-06-16 14:01:33 +0200

    update database

--- src/Database.java
+++ src/Database.java
@@ -1,8 +1,22 @@
 import java.sql.*;
 
 public class Database {
-	private String requete;
-	public void sql(){
+	private String[] champs;
+	private String[] table;
+	private String condition;
+	
+	public Database(String[] champs, String[] table, String condition){
+		this.champs = champs;
+		this.table = table;
+		this.condition = condition;
+	}
+	
+	public void requeteSlt(String[] champs, String[] table, String condition){
+		String ch = "";
+		for(int i = 0; i< champs.length-1; i++){
+			ch += champs[i] + ", "; 
+		}
+		ch += champs[champs.length];
 		try { 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 		} catch (ClassNotFoundException e) {
@@ -13,13 +27,10 @@ public class Database {
 		try{
 			Connection vCon = DriverManager.getConnection("jdbc:oracle:thin:@bd11:1521:bd11", "infs2_prj06","azerty01");
 			Statement vSt = vCon.createStatement();
-			ResultSet vRs = vSt.executeQuery("SELECT id_joueur, pseudo, couleur FROM JOUEUR");
+			ResultSet vRs = vSt.executeQuery("SELECT " + champs + " FROM " + table);
 			while(vRs.next())
-			{
-				String id = vRs.getString(1);
-				String pseudo = vRs.getString(2);
-				String couleur = vRs.getString(3);
-				System.out.println(id + pseudo + couleur);
+			{	
+				System.out.println();
 			}
 		}
 		catch(Exception e){
