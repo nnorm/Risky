071d3ea4e06083dc1ff1f3e083ab26d3ebef1ca8
Andréas BALLIS <balli002@pc081.iut-rcc-info.urca>
2015-06-17 15:30:05 +0200

    Started to complete class Interface

--- src/Interface.java
+++ src/Interface.java
@@ -1,8 +1,56 @@
 import java.util.*;
 
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
 /**
  * Classe qui défini les fonctions liées à l'interface graphique 
  * */
-public class Interface {
-
+@SuppressWarnings("serial")
+public class Interface extends JFrame implements ActionListener{
+	private JPanel panel;
+	private JButton buttontest;
+	
+	public Interface(String title, int width, int height)
+	{
+		super();
+		
+		build(title, width, height);
+	}
+	
+	private void build(String title, int width, int height)
+	{
+		setTitle(title);
+		setSize(width, height);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		this.panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		panel.setBackground(Color.red);
+		
+		JLabel labeltest = new JLabel("Fenetre de jeu");
+		panel.add(labeltest);
+		
+		this.buttontest = new JButton("clicky clicky, YOU BITCH");
+		buttontest.addActionListener(this);
+		panel.add(buttontest);
+		
+		setContentPane(panel);
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource() == buttontest)
+			System.out.println("BITCH");
+	}
+	
+	
+	public void display()
+	{
+		setVisible(true);
+	}
 }
--- src/main.java
+++ src/main.java
@@ -8,11 +8,14 @@ public class main {
  
 	public static void main(String[] argv) {
 
-		String[] champs = {"id_joueur","id_partie", "couleur", "score"};
+	/*	String[] champs = {"id_joueur","id_partie", "couleur", "score"};
 		String[] table = {"PARTICIPER"};
 		String[] val = {"J001","P001","blue", "3000"};
 		Database bd = new Database(champs,table,val,"");
-		bd.requeteInsert(); /* LANCE UNE EXCEPTION */
-		bd.requeteSlt(false);
+		bd.requeteInsert(); /* LANCE UNE EXCEPTION 
+		bd.requeteSlt(false);  */
+		/* Tests de l'interface */
+		Interface ihm = new Interface("Risky", 800, 600);
+		ihm.display();
 	}
 }
