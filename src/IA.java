b64592c309b242a8da2bd1752fbbae0d1864372a
Louis GAGOLA <gagol001@pc082.iut-rcc-info.urca>
2015-06-17 11:22:47 +0200

    changer l ia et joueur

--- src/IA.java
+++ src/IA.java
@@ -4,9 +4,13 @@ import java.util.*;
  * Classe fille de Joueur jouant le rôle d'IA. 
  * */
 public class IA extends Joueur
-{
-	private int lvlDifficulte;
+{	/*	lvlDifficulte=1 ---> niveau (tres) facile
+		
+ 	*/
+	private int lvlDifficulte;   
 	public static int nbIA = 0;
+	
+	
 	/**
 	 * Constructeur avec arguments.
 	 * @param nbArmeesInit le nombre d'armées disponible à l'initialisation du jeu. (int) 
@@ -24,9 +28,61 @@ public class IA extends Joueur
 	}
 	
 	/**
-	 * Méthode d'instance faisant jouer le tour à l'IA courante. 
-	 * */
-	public void jouerTour()
-	{}
+	 * setteur du lvl de l ia
+	 * @param lvl
+	 */
+	public void setlvlDiff(int lvl){
+		this.lvlDifficulte=lvl;
+	}
 	
+	/**
+	 * methode qui distribue les armees en fonction du lvl
+	 */
+	public void jouerDeb(){
+		this.mettre1ArmePays(this.pays);
+		if(this.lvlDifficulte==1){      // distribution 1 unite au premier pays lvl 1
+			int i=0;
+			while(this.armeesDispo !=0){
+				this.pays.get(i).ajouterPions(1);
+				this.armeesDispo=this.armeesDispo-1;
+			}
+		}
+	}
+	
+	/**
+	 * ajoute a l'arme dispo le bonus
+	 */
+	public void BonusCarte(){
+		while(this.main.size()>=3){
+			for(int i =0;i<=(this.main.size()-2);i++){
+				for(int j=1;j<=(this.main.size()-1);j++){
+					for(int p=2;p<=this.main.size();i=+3){
+						if(this.combinaison(this.main.get(i), this.main.get(j),this.main.get(p)) !=0){
+							this.armeesDispo+=this.combinaison(this.main.get(i), this.main.get(j),this.main.get(p));
+							this.main.get(i);
+							
+						}
+						
+					}
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 *  joue un tour en fonction de lvl
+	 *  
+	 */
+	public void unTour(){
+		if(this.lvlDifficulte==1){
+			this.ajNbUniteContinent();
+			this.armeesDispo+= 
+			for(int i =0;this.armeesDispo !=0;i++){
+				this.pays.get(i).ajouterPions(1);
+				this.armeesDispo=this.armeesDispo-1;
+			}
+			
+		}
+	}
 }
